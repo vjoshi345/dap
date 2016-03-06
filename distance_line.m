bc9da263e356398dcddad2c1a10db625f370a85f
vjoshi345 <vjoshi345@gmail.com>
2016-03-06 01:11:55 -0500

    modified code ot save plots; included plots of avg dist and count of inactive points

--- distance_line.m
+++ distance_line.m
@@ -1,7 +1,7 @@
-function [dist, index] = distance_line(U, P)
-%distance_line - Given matrices U and P, this function gives the largest 
-%distance from points in P to the closest line segment induced by pairs of
-%points in U.  
+function dist = distance_line(U, P)
+%distance_line - Given matrices U and P, this function gives the distance 
+%from points in P to the closest line segment induced by pairs of points
+%in U.  
 % U = matrix of the set of points (column vectors)
 % P = matrix of the set of points (column vectors) 
 % dist = the evaluated distance
@@ -13,7 +13,8 @@ n = size(P, 2); % Number of points
 
 if k == 1
     [~, temp] = point_to_line(P, U, U);
-    [dist, index] = max(temp);
+    %[dist, index] = max(temp);
+    dist = temp;
 else
     lin_seg_count = k*(k-1)/2;
     lineseg_dist = zeros(lin_seg_count, n); % Distance from each point to 
@@ -24,10 +25,11 @@ else
             [~, lineseg_dist(i, :)] = point_to_line(P, U(:, b), U(:, c));
         end
     end
-
-    min_lineseg_dist = min(lineseg_dist, [], 1); % Vector of closest 
+    
+    dist = min(lineseg_dist, [], 1);
+    %min_lineseg_dist = min(lineseg_dist, [], 1); % Vector of closest 
                                                  % distances for each point
-    [dist, index] = max(min_lineseg_dist);
+    %[dist, index] = max(min_lineseg_dist);
 end
 end
 
--- plot_point_wise.m
+++ plot_point_wise.m
@@ -4,94 +4,118 @@
 % using three different algorithms as a function of the size of the subset.
 
 tic;
-% Setting Constants
-epsilon = 1;
 
-% Importing data and converting to the matrix form
-P = csvread('ionosphere_mod.csv');
-P = P'; 
-%P = P(:, 1:50);
-[d, n] = size(P);
+for iter = 1:1
+    clearvars -except iter;
+    % Setting Constants
+    epsilon = 1;
 
-Q = P; % We will manipuate P and keep a copy of it in Q for later use
+    % Importing data and converting to the matrix form
+    P = csvread('ionosphere_mod.csv');
+    P = P'; 
+    %P = P(:, 1:50);
+    [d, n] = size(P);
 
-%%%%%%%%%%%%%%%%%% ALGORITHM-1 Point-wise max distance %%%%%%%%%%%%%%%%%%%%
-% Computing columns of U 
-r = randi([1, n], 1, 1);
-U = zeros(d, n);
-U(:, 1) = P(:, r);
-%P = P(:, [(1:r-1), (r+1:end)]);
-P(:, r) = [];
+    Q = P; % We will manipuate P and keep a copy of it in Q for later use
 
-dist_array = zeros(1, n);
-[D, ~] = pdist2((U(:, 1))', P', 'euclidean', 'Smallest', 1);
-[max_dist, max_index] = max(D);
-dist_array(1) = max_dist;
+    %%%%%%%%%%%%%%%%%% ALGORITHM-1 Point-wise max distance %%%%%%%%%%%%%%%%%%%%
+    % Computing columns of U 
+    r = randi([1, n], 1, 1);
+    U = zeros(d, n);
+    U(:, 1) = P(:, r);
+    %P = P(:, [(1:r-1), (r+1:end)]);
+    P(:, r) = [];
 
-avg_dist_array = zeros(1, n);
-avg_dist_array(1) = mean(D);
+    dist_array = zeros(1, n);
+    [D, ~] = pdist2((U(:, 1))', P', 'euclidean', 'Smallest', 1);
+    [max_dist, max_index] = max(D);
+    dist_array(1) = max_dist;
 
-flag = 0;
-for i = 2:n
-    if max_dist <= epsilon
-        flag = 1;
-        break
-    else
-        U(:, i) = P(:, max_index);
-        P(:, max_index) = [];
-        if i == n
-            dist_array(i) = 0;
+    avg_dist_array = zeros(1, n);
+    avg_dist_array(1) = mean(D);
+
+    count_inactive = zeros(1, n);
+    count_inactive(1) = sum(D <= epsilon);
+
+    flag = 0;
+    for i = 2:n
+        if max_dist <= epsilon
+            flag = 1;
+            break
         else
-            [D, ~] = pdist2((U(:, 1:i))', P', 'euclidean', 'Smallest', 1);
-            [max_dist, max_index] = max(D);
-            dist_array(i) = max_dist;
-            avg_dist_array(i) = mean(D);
+            U(:, i) = P(:, max_index);
+            P(:, max_index) = [];
+            if i == n
+                dist_array(i) = 0;
+                avg_dist_array(i) = 0;
+                count_inactive(i) = n;
+            else
+                [D, ~] = pdist2((U(:, 1:i))', P', 'euclidean', 'Smallest', 1);
+                [max_dist, max_index] = max(D);
+                dist_array(i) = max_dist;
+                avg_dist_array(i) = mean(D);
+                count_inactive(i) = sum(D <= epsilon);
+            end
         end
+        fprintf('End of iteration:%d\n', i);
     end
-    fprintf('End of iteration:%d\n', i);
+
+    if flag == 1
+        U = U(:, 1:(i-1));
+        dist_array  = dist_array(1:(i-1));
+        avg_dist_array  = avg_dist_array(1:(i-1));
+        count_inactive = count_inactive(1:(i-1));
+    else
+        U = U(:, 1:i);
+        dist_array = dist_array(1:i);
+        avg_dist_array  = avg_dist_array(1:i);
+        count_inactive = count_inactive(1:i);
+    end
+
+    %%% Plotting the distance as a function of the size of subset chosen
+    Point_wise = figure('visible', 'off');
+
+    subplot(2, 1, 1);
+    plot(dist_array);
+    title(['Point-wise distance, epsilon = ' num2str(epsilon)]);
+    xlabel('No. of points in U');
+    hold on;
+    plot(avg_dist_array, 'green');
+    legend('Max dist', 'Avg dist');
+    hold off;
+
+    subplot(2, 1, 2);
+    plot(count_inactive);
+    title(['Count of inactive points in P (<= epsilon), epsilon = ' num2str(epsilon)]);
+    xlabel('No. of points in U');
+    legend('Inactive points');
+    display(iter);
+    saveas(Point_wise, ['Output\Max_avg_inactive\Point-wise' num2str(iter) '_epsilon=' num2str(epsilon) '.jpg']);
+
+    % Evaluate X using matrix multiplication
+    P = Q; % Since P was changed in computing U, we need to reaasign
+    c = 0.1;
+    U = U + c*eye(size(U));
+    X = U\P;
+
+    % Cost (Frobenius norm) ||P - U*X||^2 
+    C = norm((P - U*X), 'fro');
+
+    % Sparsity
+    nonzero_count = sum(sum(abs(X) > 1e-5));
+    sparsity_coeff = nonzero_count/numel(X);
+
+    % % Output
+    % fprintf('No. of points(n): %d\t', n);
+    % fprintf('No. of dimensions(d): %d\n', d);
+    % fprintf('No. of clusters(k): %d\n', size(U, 2));
+    % fprintf('Cost ||P - UX||: %3.3f\n', C);
+    % fprintf('Sparsity: %3.3f\n', sparsity_coeff);
+
+    % Output - saved in csv file
+    output = [n, d, r, size(U, 2), C, sparsity_coeff, epsilon];
+    dlmwrite('Output\Max_avg_inactive\results_point.csv', output, '-append');
 end
-
-if flag == 1
-    U = U(:, 1:(i-1));
-    dist_array  = dist_array(1:(i-1));
-    avg_dist_array  = avg_dist_array(1:(i-1));
-else
-    U = U(:, 1:i);
-    dist_array = dist_array(1:i);
-    avg_dist_array  = avg_dist_array(1:i);
-end
-
-%%% Plotting the distance as a function of the size of subset chosen
-plot(dist_array);
-title(['Point-wise distance, epsilon = ' num2str(epsilon)]);
-xlabel('No. of points in U');
-hold on;
-plot(avg_dist_array, 'green');
-legend('Max dist', 'Avg dist');
-
-% Evaluate X using matrix multiplication
-P = Q; % Since P was changed in computing U, we need to reaasign
-c = 0.1;
-U = U + c*eye(size(U));
-X = U\P;
-
-% Cost (Frobenius norm) ||P - U*X||^2 
-C = norm((P - U*X), 'fro');
-
-% Sparsity
-nonzero_count = sum(sum(abs(X) > 1e-5));
-sparsity_coeff = nonzero_count/numel(X);
-
-% % Output
-% fprintf('No. of points(n): %d\t', n);
-% fprintf('No. of dimensions(d): %d\n', d);
-% fprintf('No. of clusters(k): %d\n', size(U, 2));
-% fprintf('Cost ||P - UX||: %3.3f\n', C);
-% fprintf('Sparsity: %3.3f\n', sparsity_coeff);
-
-% Output - saved in csv file
-%output = [n, d, size(U, 2), C, sparsity_coeff, epsilon];
-%dlmwrite('Output\results_point.csv', output, '-append');
 
 
 toc;
