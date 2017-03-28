ee8bea08cdcf3fdedb727ddfa8ca7ecf3f93ca61
Varun Joshi <vjoshi345@gmail.com>
2017-03-28 02:29:47 -0400

    - included the NYC_SAT_2010 dataset - deskewed MNIST data using moments method - generated results using deskewed MNIST data (commression, dictionary, reconstruction visualization) - corrected a bug in visualize_reconstruction

--- src/run_vis_recon.m
+++ src/run_vis_recon.m
@@ -1,9 +1,7 @@
-load('C:\CMU\CMU-Spring-2016\DAP\working-directory\dap\output\to_publish\Visualizations\Dictionary\mnist1-ft_Dictionary_dch-max.mat');
-Y = csvread('C:\CMU\CMU-Spring-2016\DAP\working-directory\dap\data\mnist1-ft.csv');
-Y = abs(Y);
-D = abs(D);
+load('C:\CMU\CMU-Spring-2016\DAP\working-directory\dap\output\to_publish\Visualizations\Dictionary\Deskewed\mnist1-deskewed_Dictionary_dch-max.mat');
+Y = csvread('C:\CMU\CMU-Spring-2016\DAP\working-directory\dap\data\mnist1-deskewed.csv');
 param.niter = 10;
-param.epsilon = 82.51119;
+param.epsilon = 4.66093;
 blah = setdiff(Y', D', 'rows')';
 visualize_reconstruction(D, blah(:, 10), 1, param);
 visualize_reconstruction(D, blah(:, [50, 73]), 1, param);
